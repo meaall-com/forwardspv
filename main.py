8fc3142fabeca071b559e3bad18031799228cea2
meaall-com <67676419+meaall-com@users.noreply.github.com>
2021-07-08 11:05:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ client = TelegramClient("session", 1497933, "c0780c2359c081e206bbb180f3050a5c")
 client.start()
 
 
-@client.on(events.NewMessage(chats=[155836063]))
+@client.on(events.NewMessage(chats=[155836063, -1001057566761, -1001087482329]))
 async def replier(event):
 	print(event)
-	await event.forward_to("@Processing_meaallhl")
+	await event.forward_to("@Processing_meaallh")
 	
 
 
