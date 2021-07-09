914a0e2b8e50fca65ef82bbf4c4512fa01a00ae8
meaall-com <67676419+meaall-com@users.noreply.github.com>
2021-07-10 02:23:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = TelegramClient("session", 1497933, "c0780c2359c081e206bbb180f3050a5c")
 client.start()
 
 
-@client.on(events.NewMessage(chats=[155836063, -1001057566761, -1001087482329]))
+@client.on(events.NewMessage(chats=[155836063, -1001057566761, -1001087482329, 583703673, -1001243996252]))
 async def replier(event):
 	print(event)
 	await event.forward_to("@Processing_meaallh")
